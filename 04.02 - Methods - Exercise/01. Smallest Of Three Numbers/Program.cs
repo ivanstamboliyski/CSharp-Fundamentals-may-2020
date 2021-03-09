d8e15c8dfd779b7c18a65c47d58fef9357be4ea2
Ivan Stamboliyski <ivanst.atis@gmail.com>
2021-03-09 21:41:00 +0200

    Add files via upload

---
+++ 04.02 - Methods - Exercise/01. Smallest Of Three Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._2.CSharpFundamentals_Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main()
+            {
+                int firstNum = int.Parse(Console.ReadLine());
+                int secondNum = int.Parse(Console.ReadLine());
+                int thirdNum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(GetSmallestNum(firstNum, secondNum, thirdNum));
+            }
+
+            static int GetSmallestNum(int firstNum, int secondNum, int thirdNum)
+            {
+                return Math.Min(firstNum, Math.Min(secondNum, thirdNum));
+            }
+        }
+    }
+}
