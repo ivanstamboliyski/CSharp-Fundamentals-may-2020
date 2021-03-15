acc2d58e35720091e177ced3505e94a3490ff8cd
Ivan Stamboliyski <ivanst.atis@gmail.com>
2021-03-15 23:20:59 +0200

    Add files via upload

---
+++ 05.03 - Lists - More Exercises/04. Mixed Up Lists/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.MixedUpLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbersTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> mixedList = new List<int>();
+
+            int end = Math.Min(numbersOne.Count, numbersTwo.Count) * 2;
+
+            for (int i = 0; i < end; i++)
+            {
+                if (i % 2 == 0)                             // 1 92 5 51 23 54 64 31 2 12 3 23 34 43
+                {
+                    mixedList.Add(numbersOne[0]);
+                    numbersOne.RemoveAt(0);
+                }
+                else if (i % 2 != 0)
+                {
+                    mixedList.Add(numbersTwo[numbersTwo.Count - 1]);
+                    numbersTwo.RemoveAt(numbersTwo.Count - 1);
+                }
+            }
+
+            int startConstrain = 0;
+            int endConstrain = 0;
+
+            if (numbersOne.Count > numbersTwo.Count)
+            {
+                startConstrain = numbersOne[0];
+                endConstrain = numbersOne[1];
+            }
+            else
+            {
+                startConstrain = numbersTwo[0];
+                endConstrain = numbersTwo[1];
+            }
+
+            List<int> output = new List<int>();
+
+            for (int i = 0; i < mixedList.Count; i++)
+            {
+                if (mixedList[i] > Math.Min(startConstrain, endConstrain) &&
+                    mixedList[i] < Math.Max(startConstrain, endConstrain))
+                {
+                    output.Add(mixedList[i]);
+                }
+            }
+
+            output.Sort();
+
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
---
+++ 05.03 - Lists - More Exercises/05. Drum Set/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.DrumSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> initialQuality = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> usedDrums = new List<int>();
+
+            for (int i = 0; i < initialQuality.Count; i++)
+            {
+                usedDrums.Add(initialQuality[i]);
+            }
+
+            string input = string.Empty;
+
+            //while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            //{
+            //    int hitPower = int.Parse(input);
+
+            //    for (int i = 0; i < usedDrums.Count; i++)
+            //    {
+            //        if (usedDrums[i] > 0)
+            //        {
+            //            usedDrums[i] -= hitPower;
+            //        }
+
+            //        if (usedDrums[i] < 1)
+            //        {
+            //            int drumPrice = initialQuality[i] * 3;
+
+            //            if (drumPrice <= savings)
+            //            {
+            //                savings -= drumPrice;
+            //                usedDrums[i] = initialQuality[i];
+            //            }
+            //            else
+            //            {
+            //                usedDrums[i] = 0;
+            //            }
+            //        }
+            //    }
+            //}
+
+            //usedDrums.RemoveAll(n => n == 0);
+
+            while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+
+                for (int i = 0; i < usedDrums.Count; i++)
+                {
+                    usedDrums[i] -= hitPower;
+
+                    if (usedDrums[i] < 1)
+                    {
+                        int drumPrice = initialQuality[i] * 3;
+
+                        if (drumPrice <= savings)
+                        {
+                            savings -= drumPrice;
+                            usedDrums[i] = initialQuality[i];
+                        }
+                        else
+                        {
+                            usedDrums.RemoveAt(i);
+                            initialQuality.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", usedDrums));
+            Console.WriteLine($"Gabsy has {savings:F2}lv.");
+        }
+    }
+}
