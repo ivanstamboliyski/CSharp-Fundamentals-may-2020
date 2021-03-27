a082fd0b56632f9b866a8c86db55c9f4a19bf834
Ivan Stamboliyski <ivanst.atis@gmail.com>
2021-03-27 17:08:51 +0200

    Add files via upload

---
+++ 09.03.RegularExpressions-MoreExercises/01.WinningTicket/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MoreExercise._01.WinningTicket
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] tickets = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                string currTicket = tickets[i].Trim();
+
+                if (currTicket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                string leftHalf = currTicket.Substring(0,10);
+                string rightHalf = currTicket.Substring(10,10);
+
+                string pattern = @"[$]{6,10}|[#]{6,10}|[\^]{6,10}|[@]{6,10}";
+
+                Match leftSide = Regex.Match(leftHalf, pattern);
+                Match rightSide = Regex.Match(rightHalf, pattern);
+
+                if (leftSide.Success && rightSide.Success)
+                {
+                    char winningSymbol = currTicket.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+                    double leftHalfOccurences = leftHalf.Count(x => x == winningSymbol);
+                    double rightHalfOccurences = rightHalf.Count(x => x == winningSymbol);
+
+                    if (leftHalfOccurences == 10 && rightHalfOccurences == 10)
+                    {
+                        Console.WriteLine($"ticket \"{currTicket}\" - {rightHalfOccurences}{winningSymbol} Jackpot!");
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"ticket \"{currTicket}\" - {Math.Min(leftHalfOccurences, rightHalfOccurences)}{winningSymbol}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"ticket \"{currTicket}\" - no match");
+                }
+            }
+        }
+    }
+}
---
+++ 09.03.RegularExpressions-MoreExercises/02.RageQuit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace MoreExercise._02.RageQuit
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine().ToUpper();
+
+            string pattern = @"(([^\d]+)(\d+))";
+
+            var matches = Regex.Matches(text, pattern);
+
+            var result = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                string temp = match.Groups[2].Value;
+
+                int repeats = int.Parse(match.Groups[3].Value);
+
+                result.AppendJoin(match.Groups[2].Value, new string[repeats + 1]);
+            }
+
+            int count = result.ToString().Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {count}");
+
+            Console.WriteLine(result);
+        }
+    }
+}
